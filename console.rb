89a9d12d83b34c6e811f8a3b445848a03690a35e
Adamsh6 <adam.s.houten@gmail.com>
2019-05-22 14:48:55 +0100

    added save function to artist

--- console.rb
+++ console.rb
@@ -1,10 +1,10 @@
 require("pry")
-require('rg')
+require('pg')
 require_relative('models/album')
 require_relative('models/artist')
 
 artist1 = Artist.new({'name' => 'The Police'})
-
+artist1.save
 
 
 
