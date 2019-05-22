f3ab3c9df1d4e6190bcf694c3807d75e3e6b84a5
Adamsh6 <adam.s.houten@gmail.com>
2019-05-22 15:23:08 +0100

    added update method to album

--- models/album.rb
+++ models/album.rb
@@ -5,6 +5,8 @@ require_relative('../db/sql_runner')
 
 class Album
 
+  attr_accessor :title, :genre
+
   def initialize(options)
     @id = options['id'] if options['id']
     @title = options['title']
@@ -18,6 +20,12 @@ class Album
     values = [@title, @genre, @artist_id]
     result = SqlRunner.run(sql, values)
     @id = result[0]['id']
+  end
+
+  def update
+    sql = "UPDATE albums SET (title, genre) = ($1, $2) WHERE id = $3"
+    values = [@title, @genre, @id]
+    SqlRunner.run(sql, values)
   end
 
   def artist
